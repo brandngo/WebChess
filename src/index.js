5f8ca27b66292aa1eb831da823db52c8d43ca608
Brandon Ngo <okibrandon@hotmail.com>
2020-12-28 17:45:12 -0500

    beginning of the project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  
+}
+
+class Game extends React.Component {}
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
